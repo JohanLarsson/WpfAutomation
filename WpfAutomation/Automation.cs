c8bb9e9ba9cec3788202374c66e1997d2d06b95b
Johan Larsson <johan_gavle1@hotmail.com>
2014-05-14 20:22:16 +0200

    Initial

---
+++ WpfAutomation/Automation.cs
@@ -0,0 +1,154 @@
+﻿namespace WpfAutomation
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Input;
+
+    public static class Automation
+    {
+        public static void SimulateClick(this CheckBox checkBox)
+        {
+            checkBox.RaiseMouseButton(UIElement.PreviewMouseLeftButtonDownEvent, MouseButton.Left, 1)
+                    .RaiseMouseButton(Mouse.PreviewMouseDownEvent, MouseButton.Left, 1)
+                    .RaiseKeyboardFocusEvent(Keyboard.PreviewGotKeyboardFocusEvent)// old: System.Windows.Controls.TextBox new: System.Windows.Controls.CheckBox Content: IsChecked:False
+                // .SetProp(UIElement.IsKeyboardFocusWithinProperty, True)
+                // .SetProp(UIElement.IsKeyboardFocusedProperty, True)
+                    .RaiseRoutedEvent(FocusManager.GotFocusEvent)
+                    .RaiseKeyboardFocusEvent(Keyboard.GotKeyboardFocusEvent) // old: System.Windows.Controls.TextBox new: System.Windows.Controls.CheckBox Content: IsChecked:False
+                // .SetProp(UIElement.IsMouseCaptureWithinProperty, True)
+                // .SetProp(UIElement.IsMouseCapturedProperty, True)
+                    .RaiseMouseEvent(Mouse.GotMouseCaptureEvent)
+                // .SetProp(UIElement.IsMouseDirectlyOverProperty, True)
+                    .RaiseMouseButton(UIElement.PreviewMouseLeftButtonUpEvent, MouseButton.Left, 1)
+                    .RaiseMouseButton(Mouse.PreviewMouseUpEvent, MouseButton.Left, 1)
+                // .SetProp(UIElement.IsMouseCaptureWithinProperty, False)
+                // .SetProp(UIElement.IsMouseCapturedProperty, False)
+                    .RaiseMouseEvent(Mouse.LostMouseCaptureEvent)
+                // .SetProp(UIElement.IsMouseDirectlyOverProperty, False)
+                    .Toggle()
+                    .RaiseRoutedEvent(ButtonBase.ClickEvent);
+        }
+
+        public static void SimulateKey(this CheckBox checkBox, Key key)
+        {
+            checkBox.RaiseKeyboardEvent(Keyboard.PreviewKeyDownEvent, Key.Space)
+                // .SetProp(UIElement.IsMouseCaptureWithinProperty, True)
+                // .SetProp(UIElement.IsMouseCapturedProperty, True)
+                    .RaiseMouseEvent(Mouse.GotMouseCaptureEvent)
+                // .SetProp(UIElement.IsMouseDirectlyOverProperty, True)
+                    .RaiseKeyboardEvent(Keyboard.PreviewKeyUpEvent, Key.Space)
+                // .SetProp(UIElement.IsMouseCaptureWithinProperty, False)
+                // .SetProp(UIElement.IsMouseCapturedProperty, False)
+                    .RaiseMouseEvent(Mouse.LostMouseCaptureEvent)
+                // .SetProp(UIElement.IsMouseDirectlyOverProperty, False)
+                    .Toggle()
+                    .RaiseRoutedEvent(ButtonBase.ClickEvent);
+        }
+
+        public static void SimulateKey(this TextBoxBase textBox, Key key)
+        {
+            textBox.RaiseKeyboardEvent(UIElement.PreviewKeyDownEvent, key)
+                   .RaiseKeyboardEvent(UIElement.KeyDownEvent, key)
+                   .RaiseKeyboardEvent(UIElement.KeyDownEvent, key)
+                   .SetKeyboardFocus()
+                   .RaiseKeyboardEvent(UIElement.PreviewKeyUpEvent, key)
+                   .RaiseKeyboardEvent(UIElement.KeyUpEvent, key);
+            textBox.AppendText(key.ToString());
+        }
+
+        public static ToggleButton Toggle(this ToggleButton toggleButton)
+        {
+            if (toggleButton.IsChecked == null)
+            {
+                toggleButton.SetProp(ToggleButton.IsCheckedProperty, false);
+            }
+            else
+            {
+                toggleButton.SetProp(ToggleButton.IsCheckedProperty, !toggleButton.IsChecked);
+            }
+            if (toggleButton.IsChecked == true)
+            {
+                toggleButton.RaiseEvent(new RoutedEventArgs(ToggleButton.CheckedEvent));
+            }
+            else
+            {
+                toggleButton.RaiseEvent(new RoutedEventArgs(ToggleButton.UncheckedEvent));
+            }
+            return toggleButton;
+        }
+
+        public static T RaiseMouseButton<T>(this T e, RoutedEvent @event, MouseButton button, int clickCount)
+            where T : UIElement
+        {
+            var arg = new MouseButtonEventArgs(Mouse.PrimaryDevice, 0, button)
+            {
+                RoutedEvent = @event
+            };
+
+            e.RaiseEvent(arg);
+            return e;
+        }
+
+        public static T RaiseKeyboardEvent<T>(this T e, RoutedEvent @event, Key key) where T : UIElement
+        {
+            var arg = new KeyEventArgs(Keyboard.PrimaryDevice, Keyboard.PrimaryDevice.ActiveSource, 0, key)
+            {
+                RoutedEvent = @event
+            };
+            e.RaiseEvent(arg);
+            return e;
+        }
+
+        public static T RaiseKeyboardFocusEvent<T>(this T e, RoutedEvent @event) where T : UIElement
+        {
+            var arg = new KeyboardFocusChangedEventArgs(Keyboard.PrimaryDevice, 0, null, e)
+            {
+                RoutedEvent = @event
+            };
+            e.RaiseEvent(arg);
+            return e;
+        }
+
+        public static T RaiseRoutedEvent<T>(this T e, RoutedEvent @event) where T : UIElement
+        {
+            var arg = new RoutedEventArgs
+            {
+                RoutedEvent = @event
+            };
+            e.RaiseEvent(arg);
+            return e;
+        }
+       
+        public static T RaiseMouseEvent<T>(this T e, RoutedEvent @event) where T : UIElement
+        {
+            var arg = new MouseEventArgs(Mouse.PrimaryDevice, 0)
+            {
+                RoutedEvent = @event
+            };
+            e.RaiseEvent(arg);
+            return e;
+        }
+        
+        public static T SetKeyboardFocus<T>(this T e) where T : UIElement
+        {
+            if (e.IsKeyboardFocused)
+            {
+                return e;
+            }
+            FocusManager.SetFocusedElement(e, Keyboard.Focus(e));
+            var arg = new RoutedEventArgs
+            {
+                RoutedEvent = UIElement.GotKeyboardFocusEvent
+            };
+            e.RaiseEvent(arg);
+            return e;
+        }
+
+        public static T SetProp<T>(this T e, DependencyProperty prop, object value) where T : UIElement
+        {
+            e.SetValue(prop, value);
+            return e;
+        }
+    }
+}
---
+++ WpfAutomation/EventsSampler.xaml.cs
@@ -0,0 +1,272 @@
+﻿namespace WpfAutomation
+{
+    using System.CodeDom.Compiler;
+    using System.IO;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Interaction logic for EventsSampler.xaml
+    /// </summary>
+    public partial class EventsSampler : UserControl
+    {
+        private StringWriter _sw = new StringWriter();
+        private IndentedTextWriter _itw;
+        public EventsSampler()
+        {
+            InitializeComponent();
+            _itw = new IndentedTextWriter(_sw);
+            SubscribeUiElement(EventTextBox, EventCheckBox);
+            SubscribeControl(EventTextBox, EventCheckBox);
+            SubscribeTextBox(EventTextBox);
+            SubscribeToggleButton(EventCheckBox);
+        }
+
+        private void SubscribeUiElement(params UIElement[] elements)
+        {
+            foreach (var e in elements)
+            {
+                e.GotFocus += OnRoutedEvent;
+                e.LostFocus += OnRoutedEvent;
+
+                e.PreviewGotKeyboardFocus += OnKeyboardFocus;
+                e.GotKeyboardFocus += OnKeyboardFocus;
+                e.PreviewLostKeyboardFocus += OnKeyboardFocus;
+                e.LostKeyboardFocus += OnKeyboardFocus;
+
+                e.PreviewKeyDown += OnKey;
+                e.KeyDown += OnKey;
+                e.PreviewKeyUp += OnKey;
+                e.KeyUp += OnKey;
+
+                e.PreviewTextInput += OnTextInput;
+                e.TextInput += OnTextInput;
+
+                e.IsKeyboardFocusedChanged += OnDpChanged;
+                e.IsKeyboardFocusWithinChanged += OnDpChanged;
+
+                e.PreviewMouseDown += OnMouseButton;
+                e.MouseDown += OnMouseButton;
+                e.PreviewMouseUp += OnMouseButton;
+                e.MouseUp += OnMouseButton;
+
+                e.PreviewMouseLeftButtonDown += OnMouseButton;
+                e.MouseLeftButtonDown += OnMouseButton;
+                e.PreviewMouseLeftButtonUp += OnMouseButton;
+                e.MouseLeftButtonUp += OnMouseButton;
+
+                e.PreviewMouseRightButtonDown += OnMouseButton;
+                e.MouseRightButtonDown += OnMouseButton;
+                e.PreviewMouseRightButtonUp += OnMouseButton;
+                e.MouseRightButtonUp += OnMouseButton;
+
+                e.PreviewMouseWheel += OnMouseWheel;
+                e.MouseWheel += OnMouseWheel;
+
+                e.MouseEnter += OnMouseEvent;
+                e.MouseLeave += OnMouseEvent;
+                e.GotMouseCapture += OnMouseEvent;
+                e.LostMouseCapture += OnMouseEvent;
+
+                e.IsMouseCapturedChanged += OnDpChanged;
+                e.IsMouseCaptureWithinChanged += OnDpChanged;
+                e.IsMouseDirectlyOverChanged += OnDpChanged;
+            }
+        }
+
+        private void SubscribeControl(params Control[] controls)
+        {
+            foreach (var c in controls)
+            {
+                c.PreviewMouseDoubleClick += OnMouseButton;
+                c.MouseDoubleClick += OnMouseButton;
+            }
+        }
+
+        private void SubscribeTextBox(TextBox tb)
+        {
+            tb.SelectionChanged += OnRoutedEvent;
+            tb.TextChanged += OnTextChanged;
+        }
+
+        private void SubscribeButton(Button b)
+        {
+            b.Click += OnRoutedEvent;
+        }
+
+        private void SubscribeToggleButton(ToggleButton tb)
+        {
+            tb.Click += OnRoutedEvent;
+            tb.Checked += OnRoutedEvent;
+            tb.Unchecked += OnRoutedEvent;
+        }
+
+        private void OnRoutedEvent(object sender, RoutedEventArgs e)
+        {
+            if (!IsSampling(sender))
+            {
+                return;
+            }
+            if (e.RoutedEvent == ToggleButton.CheckedEvent || e.RoutedEvent == ToggleButton.UncheckedEvent)
+            {
+                _itw.WriteLine(".Toggle()");
+            }
+            else
+            {
+                _itw.WriteLine(".RaiseRoutedEvent({0})", e.RoutedEvent.OwnerType.Name + "." + e.RoutedEvent.Name + "Event");
+            }
+
+            CodeBox.Text = _sw.ToString();
+        }
+
+        private void OnDpChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (!IsSampling(sender))
+            {
+                return;
+            }
+            string s = string.Format(".SetProp({0}, {1})", e.Property.OwnerType.Name + "." + e.Property.Name + "Property", e.NewValue);
+            if (e.Property.ReadOnly)
+            {
+                _itw.WriteLine(@"// " + s);
+            }
+            else
+            {
+                _itw.WriteLine(s);
+            }
+
+            CodeBox.Text = _sw.ToString();
+        }
+
+        private void OnKey(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (!IsSampling(sender))
+            {
+                return;
+            }
+            var code = string.Format(".RaiseKeyboardEvent({0}, Key.{1})", e.RoutedEvent.OwnerType.Name + "." + e.RoutedEvent.Name + "Event", e.Key);
+
+            if (e.RoutedEvent == UIElement.PreviewKeyDownEvent)
+            {
+                _itw.WriteLine();
+                var name = ToFirstCharLower(sender.GetType().Name);
+                _itw.WriteLine(name + code);
+                _itw.Indent = 3;
+            }
+            else
+            {
+                _itw.WriteLine(code);
+            }
+            CodeBox.Text = _sw.ToString();
+        }
+
+        private void OnKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+        {
+            if (!IsSampling(sender))
+            {
+                return;
+            }
+            //RaiseKeyboardFocusEvent
+            _itw.WriteLine(".RaiseKeyboardFocusEvent({0}) // old: {1} new: {2}", e.RoutedEvent.OwnerType.Name + "." + e.RoutedEvent.Name + "Event", e.OldFocus, e.NewFocus);
+            CodeBox.Text = _sw.ToString();
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!IsSampling(sender))
+            {
+                return;
+            }
+            _itw.WriteLine(".RaiseTextChangedEvent({0})", e.RoutedEvent.OwnerType.Name + "." + e.RoutedEvent.Name + "Event");
+            CodeBox.Text = _sw.ToString();
+        }
+
+        private void OnTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            if (!IsSampling(sender))
+            {
+                return;
+            }
+            _itw.WriteLine(".RaiseTextInputEvent({0})", e.RoutedEvent.OwnerType.Name + "." + e.RoutedEvent.Name + "Event");
+            CodeBox.Text = _sw.ToString();
+        }
+
+        private void OnMouseButton(object sender, MouseButtonEventArgs e)
+        {
+            if (!IsSampling(sender))
+            {
+                return;
+            }
+            var code = string.Format(".RaiseMouseButton({0}, {1}, {2})", e.RoutedEvent.OwnerType.Name + "." + e.RoutedEvent.Name + "Event", "MouseButton." + e.ChangedButton, e.ClickCount);
+
+            if (e.RoutedEvent == PreviewMouseLeftButtonDownEvent||
+                e.RoutedEvent == PreviewMouseDoubleClickEvent || 
+                e.RoutedEvent == PreviewMouseRightButtonDownEvent)
+            {
+                _itw.WriteLine();
+                var name = ToFirstCharLower(sender.GetType().Name);
+                _itw.WriteLine(name + code);
+                _itw.Indent = 3;
+            }
+            else
+            {
+                _itw.WriteLine(code);
+
+            }
+            CodeBox.Text = _sw.ToString();
+        }
+
+        private void OnMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (!IsSampling(sender))
+            {
+                return;
+            }
+            _itw.WriteLine(".RaiseMouseWheel({0}, {1})", e.RoutedEvent.OwnerType.Name + "." + e.RoutedEvent.Name + "Event", e.Delta);
+            CodeBox.Text = _sw.ToString();
+        }
+
+        private void OnMouseEvent(object sender, MouseEventArgs e)
+        {
+            if (!IsSampling(sender))
+            {
+                return;
+            }
+            if (e.RoutedEvent == UIElement.MouseEnterEvent || e.RoutedEvent == UIElement.MouseLeaveEvent)
+            {
+                _itw.Indent = 0;
+            }
+            _itw.WriteLine(".RaiseMouseEvent({0})", e.RoutedEvent.OwnerType.Name + "." + e.RoutedEvent.Name + "Event");
+            CodeBox.Text = _sw.ToString();
+        }
+
+        private void ClearClick(object sender, RoutedEventArgs e)
+        {
+            _sw.Dispose();
+            _sw = new StringWriter();
+            _itw = new IndentedTextWriter(_sw);
+            CodeBox.Text = "";
+        }
+
+        private bool IsSampling(object sender)
+        {
+            if (SampleCheckBox.IsChecked == true && ReferenceEquals(sender, EventCheckBox))
+            {
+                return true;
+            }
+            if (SampleTextBox.IsChecked == true && ReferenceEquals(sender, EventTextBox))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static string ToFirstCharLower(string s)
+        {
+            return new string(s.Take(1).Select(char.ToLower).Concat(s.Skip(1)).ToArray());
+        }
+    }
+}
